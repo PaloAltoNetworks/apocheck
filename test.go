7a25645a2cc31a41d36cb6ad50a36a48214a0bf5
Satyam Sinha <satyam@aporeto.com>
2018-05-29 19:38:36 -0700

    Add support for variant

--- test.go
+++ test.go
@@ -12,8 +12,11 @@ type Test struct {
 	Description string
 	Author      string
 	Tags        []string
-	Variant     string
-	Variants    map[string]interface{}
+
+	// To allow reusability of test code, we allow variants which can run the same test
+	// multiple times, once for each variant with the information stored in the map.
+	currentVariant string
+	Variants       map[string]interface{}
 
 	Setup    SetupFunction
 	Function TestFunction
@@ -28,8 +31,12 @@ categories : %s
 `, t.id, t.Name, t.Description, t.Author, strings.Join(t.Tags, ", "))
 }
 
-// GetVariants retrieves variants of the test
-func (t Test) GetVariants() (string, interface{}) {
+// GetVariant retrieves current variant of the test
+func (t Test) GetVariant() (string, interface{}) {
 
-	return t.Variant, t.Variants[t.Variant]
+	if t.Variants == nil {
+		return "", nil
+	}
+
+	return t.currentVariant, t.Variants[t.currentVariant]
 }
